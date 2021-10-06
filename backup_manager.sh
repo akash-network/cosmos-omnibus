bb4e241651c921c90e6430891dd34a2b3720816b
Tom Beynon <tom@tombeynon.co.uk>
2021-10-06 15:26:13 +0100

    Set expected size for large uploads

--- backup_manager.sh
+++ backup_manager.sh
@@ -3,7 +3,8 @@
 set -e
 
 COMMAND="${COMMAND:-$PROJECT_CMD}"
-RUN_AT="${RUN_AT:-20:33:00}"
+RUN_AT="${RUN_AT:-00:00:00}"
+EXPECTED_SIZE="${EXPECTED_SIZE}"
 BACKUP_DIR="${BACKUP_DIR:-$PROJECT_HOME/data}"
 BACKUP_PATH="${BACKUP_PATH}"
 BACKUP_RETAIN="${BACKUP_RETAIN:-2 days}"
@@ -27,7 +28,7 @@ while true; do
         timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
         s3_uri="${s3_uri_base}/backup_${timestamp}.tar.gz"
 
-        tar c $BACKUP_DIR | gzip | aws $aws_args s3 cp - "$s3_uri"
+        tar c $BACKUP_DIR | gzip | aws $aws_args s3 cp - "$s3_uri" --expected-size $EXPECTED_SIZE
 
         echo "$DATE: Deleting backups older than $BACKUP_RETAIN"
         aws $aws_args s3 ls "${s3_uri_base}/backup_" | while read -r line; do
