943534e0dc0d53291607b9956583939b50fbb73a
Tom Beynon <tom@tombeynon.co.uk>
2021-10-12 23:10:23 +0100

    Add RUN_ON variable to control backup day of week

--- backup_manager.sh
+++ backup_manager.sh
@@ -4,25 +4,28 @@ set -e
 
 COMMAND="${COMMAND:-$PROJECT_CMD}"
 RUN_AT="${RUN_AT:-00:00:00}"
+RUN_ON="${RUN_ON:-*}"
 EXPECTED_SIZE="${EXPECTED_SIZE}"
 BACKUP_DIR="${BACKUP_DIR:-$PROJECT_HOME/data}"
 BACKUP_PATH="${BACKUP_PATH}"
 BACKUP_RETAIN="${BACKUP_RETAIN:-2 days}"
-DATE=`date -u +%T`
+TIME=$(date -u +%T)
+DOW=$(date +%u)
 
-echo "$DATE: Starting server"
-echo "$DATE: Backup will run at $RUN_AT"
+echo "$TIME: Starting server"
+echo "$TIME: Backup will run at $RUN_AT on day $RUN_ON"
 $COMMAND &
 PID=$!
 
 while true; do
-    DATE=`date -u +%T`
-    if [[ $DATE == $RUN_AT ]]; then
-        echo "$DATE: Stopping server"
+    TIME=$(date -u +%T)
+    DOW=$(date +%u)
+    if [[ ($RUN_ON == "*") || ($RUN_ON == $DOW) ]] && [[ $TIME == $RUN_AT ]]; then
+        echo "$TIME: Stopping server"
         kill -15 $PID
         wait
 
-        echo "$DATE: Running backup"
+        echo "$TIME: Running backup"
         aws_args="--endpoint-url https://s3.filebase.com"
         s3_uri_base="s3://${BACKUP_PATH}"
         timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
@@ -30,7 +33,7 @@ while true; do
 
         tar c $BACKUP_DIR | gzip | aws $aws_args s3 cp - "$s3_uri" --expected-size $EXPECTED_SIZE
 
-        echo "$DATE: Deleting backups older than $BACKUP_RETAIN"
+        echo "$TIME: Deleting backups older than $BACKUP_RETAIN"
         aws $aws_args s3 ls "${s3_uri_base}/backup_" | while read -r line; do
             createDate=`echo $line|awk {'print $1" "$2'}`
             createDate=`date -d"$createDate" +%s`
@@ -43,7 +46,7 @@ while true; do
             fi
         done;
 
-        echo "$DATE: Restarting server"
+        echo "$TIME: Restarting server"
         $COMMAND &
         PID=$!
         sleep 1s
