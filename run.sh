5a1fdf65fd18a247e7272c669dcc24eda421ddca
Tom Beynon <tom@tombeynon.co.uk>
2024-11-11 16:08:43 +0000

    Exit if snapshot restore fails

--- run.sh
+++ run.sh
@@ -304,9 +304,9 @@ if [ "$DOWNLOAD_SNAPSHOT" == "1" ]; then
       STORJ_SNAPSHOT_URL=${SNAPSHOT_URL#*link.storjshare.io/s/}
       STORJ_SNAPSHOT_URL=${STORJ_SNAPSHOT_URL#*/}
       STORJ_SNAPSHOT_URL=${STORJ_SNAPSHOT_URL%%\?*}
-      (uplink cp $storj_args sj://${STORJ_SNAPSHOT_URL} - | pv -petrafb -i 5 $pv_extra_args | eval $tar_cmd) 2>&1 | stdbuf -o0 tr '\r' '\n'
+      (uplink cp $storj_args sj://${STORJ_SNAPSHOT_URL} - | pv -petrafb -i 5 $pv_extra_args | eval $tar_cmd) 2>&1 | stdbuf -o0 tr '\r' '\n' || exit 1
     else
-      (wget -nv -O - $SNAPSHOT_URL | pv -petrafb -i 5 $pv_extra_args | eval $tar_cmd) 2>&1 | stdbuf -o0 tr '\r' '\n'
+      (wget -nv -O - $SNAPSHOT_URL | pv -petrafb -i 5 $pv_extra_args | eval $tar_cmd) 2>&1 | stdbuf -o0 tr '\r' '\n' || exit 1
     fi
 
     [ -n "${SNAPSHOT_DATA_PATH}" ] && mv ./${SNAPSHOT_DATA_PATH}/* ./ && rm -rf ./${SNAPSHOT_DATA_PATH}
