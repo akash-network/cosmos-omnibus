bb8ddb5a41edb0ec142100c6046188423c2c44d3
Playwo <94938310+PoolPirate@users.noreply.github.com>
2025-03-03 00:58:25 +0100

    fix: crash on missing peers in chain metadata (#1008)

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,8 +20,8 @@ fi
 if [[ $CHAIN_JSON_EXISTS == true ]]; then
   sleep 0.5 # avoid rate limiting
   CHAIN_METADATA=$(curl -Ls $CHAIN_JSON)
-  CHAIN_SEEDS=$(echo $CHAIN_METADATA | jq -r '.peers.seeds | map(.id+"@"+.address) | join(",")')
-  CHAIN_PERSISTENT_PEERS=$(echo $CHAIN_METADATA | jq -r '.peers.persistent_peers | map(.id+"@"+.address) | join(",")')
+  CHAIN_SEEDS=$(echo $CHAIN_METADATA | jq -r '.peers.seeds? // [] | map(.id+"@"+.address) | join(",")')
+  CHAIN_PERSISTENT_PEERS=$(echo "$CHAIN_METADATA" | jq -r '.peers.persistent_peers? // [] | map(.id+"@"+.address) | join(",")')
 
   export CHAIN_ID="${CHAIN_ID:-$(echo $CHAIN_METADATA | jq -r .chain_id)}"
   export GENESIS_URL="${GENESIS_URL:-$(echo $CHAIN_METADATA | jq -r '.codebase.genesis.genesis_url? // .genesis.genesis_url? // .genesis?')}"
